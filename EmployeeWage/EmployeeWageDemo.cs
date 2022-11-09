6f4e5b2270f0974911058f1116fb42b91cdcab5f
dericklopis <stanforever08@gmail.com>
2022-11-09 09:25:04 +0530

    Added: EmployeeAttendence

---
+++ EmployeeWage/EmployeeWageDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class EmployeeWageDemo
+    {
+        static void Main(string[] args)
+        {
+            //master
+            Console.WriteLine("Welcome to EmployeeWageDemo");
+            //UC1
+            int employeePresent = 1;
+            Random r = new Random();
+            int employeeImput = r.Next(0, 2);
+
+            if (employeePresent == employeeImput)
+            Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+                Console.ReadLine();
+        }
+    }
+}
