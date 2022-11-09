5219212064896f42969b77160a63d9ce95c1dbe5
dericklopis <stanforever08@gmail.com>
2022-11-09 19:44:30 +0530

    Added: CalculateDailyWage

--- EmployeeWage/EmployeeWageDemo.cs
+++ EmployeeWage/EmployeeWageDemo.cs
@@ -10,18 +10,26 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            //master
-            Console.WriteLine("Welcome to EmployeeWageDemo");
-            //UC1
-            int employeePresent = 1;
-            Random r = new Random();
-            int employeeImput = r.Next(0, 2);
-
-            if (employeePresent == employeeImput)
-            Console.WriteLine("Employee is Present");
+            //Constants
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //Variables 
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
             else
-                Console.WriteLine("Employee is Absent");
-                Console.ReadLine();
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+            Console.ReadLine();
         }
     }
 }
